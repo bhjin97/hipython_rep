cd5d41d2521e76867fe37f902a7343c7c8f39321
bhjin97 <baehj0907@gmail.com>
2025-06-12 12:52:09 +0900

    '파이썬 연습'

---
+++ first.py
@@ -0,0 +1,18 @@
+print('하이 first module입니다')
+
+def my_func():
+    print('hello')
+
+def my_func1():
+    print('2nd hello')
+
+def my_func2():
+    print('3nd hello')
+
+print('바이 first module입니다')
+#my_func()
+# run module
+
+
+if__name__ == "__main__":
+    my func()
